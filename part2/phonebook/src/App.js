2863fadbc52e5eadb4fade4124086f2649d867b2
PJJ <puayjianjun@gmail.com>
2022-08-08 22:38:09 +0800

    Completed part2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,28 @@
 import Persons from './components/Persons'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook,[])
+
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
-    // const names = persons.map(person => person.name)
     
     if(persons.map(person => person.name).indexOf(newName) === -1) {
       const personObject = {
