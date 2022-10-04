1c6eff5701544a11bd1d87e2504d05e32c2167b1
PJJ <puayjianjun@gmail.com>
2022-10-04 22:47:57 +0800

    Completed Exercise 9.1

---
+++ part9/learntypescript/bmiCalculator.ts
@@ -0,0 +1,15 @@
+const calculateBmi = (height: number, weight: number): string => {
+  let h: number = height / 100;
+  let bmi: number = weight / (h * h)
+  if (bmi < 18.5) {
+    return "Underweight"
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "Normal (healthy weight)"
+  } else if (bmi >= 25 && bmi < 30) {
+    return "Overweight"
+  } else {
+    return "Obese"
+  }
+}
+
+console.log(calculateBmi(180, 74))
